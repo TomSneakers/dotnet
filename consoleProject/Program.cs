3aeb7fcab20021e15ebe31aa5ed10f2ba6339fed
Tom <tomdesvignes031@gmail.com>
2023-12-11 12:00:31 +0100

    maj

--- consoleProject/Program.cs
+++ consoleProject/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("test function sum");
+
+        
 
         Console.WriteLine("Press A to exit.");
         while (Console.ReadKey().Key != ConsoleKey.A)
@@ -12,5 +14,26 @@ class Program
             Console.WriteLine("Non recognized key. Press A to exit.");
         }
 
+    }
+
+    public static int Sum(IEnumerable<object> values)
+    {
+        int sum = 0;
+        foreach (var value in values)
+        {
+            switch (value)
+            {
+                case 0:
+                    break;
+                case int val:
+                    sum += val;
+                    break;
+                case IEnumerable<object> subList when subList.Any():
+                    sum += Sum(subList);
+                    break;
+
+            }
+        }
+        return sum;
     }
 }
