ab02cacb525335701fbb46c217b421974ca79286
Tom <tomdesvignes031@gmail.com>
2023-12-11 17:16:49 +0100

    maj

--- BookStoreAPI/Controllers/BookController.cs
+++ BookStoreAPI/Controllers/BookController.cs
@@ -2,79 +2,89 @@ using System.Collections.Generic;
 using BookStoreAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-
-namespace BookStoreAPI.Controllers; // BookStoreAPI est l'espace de nom racine de mon projet 
-
-
-
-[ApiController]
-public class BookController : ControllerBase
+namespace BookStoreAPI.Controllers
 {
-    //Methode GET
-    [HttpGet]
-    //Route permet de definir l'url de la methode
+    [ApiController]
     [Route("api/books")]
-    public ActionResult<List<Book>> GetBooks()
+    public class BookController : ControllerBase
     {
-        var books = new List<Book>
+        private readonly List<Book> _books;
+
+        public BookController()
         {
-            new() { Id = 1, Title = "Elon Musk", Author = "Elon musk" },
-        };
-
-        return books;
-    }
-    //Methode POST
-    [HttpPost]
-    //Route permet de definir l'url de la methode
-    [Route("api/books")]
-    public ActionResult<Book> CreateBook(Book book)
-    {
-
-        var newBook = new Book
-        {
-            Id = 2,
-            Title = book.Title,
-            Author = book.Author
-        };
-
-        return newBook;
-    }
-
-    //Methode PUT
-    [HttpPut]
-    //Route permet de definir l'url de la methode
-    [Route("api/books/{id}")]
-    public ActionResult<Book> UpdateBook(int id, Book book)
-    {
-        var newBook = new Book
-        {
-            Id = id,
-            Title = book.Title,
-            Author = book.Author
-        };
-
-        return newBook;
-    }
-
-    // DELETE api/books/{id}
-    [Route("api/books/{id}")]
-    public ActionResult DeleteBook(int id)
-    {
-        var existingBook = _books.Find(b => b.Id == id);
-
-        if (existingBook == null)
-        {
-            return NotFound();
+            _books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Elon Musk", Author = "Elon Musk" }
+            };
         }
 
-        _books.Remove(existingBook);
+        // GET api/books
+        [HttpGet]
+        [Route("api/books")]
 
-        return Ok();
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return _books;
+        }
+
+        // POST api/books
+        [HttpPost]
+        [Route("api/books")]
+
+        public ActionResult<Book> CreateBook(Book book)
+        {
+            var newBook = new Book
+            {
+                Id = GenerateNewId(),
+                Title = book.Title,
+                Author = book.Author
+            };
+
+            _books.Add(newBook);
+
+            return newBook;
+        }
+
+        // PUT api/books/{id}
+        [HttpPut]
+        [Route("api/books")]
+
+        public ActionResult<Book> UpdateBook(int id, Book book)
+        {
+            var existingBook = _books.Find(b => b.Id == id);
+
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            existingBook.Title = book.Title;
+            existingBook.Author = book.Author;
+
+            return existingBook;
+        }
+
+        // DELETE api/books/{id}
+        [HttpDelete]
+        [Route("api/books")]
+
+        public ActionResult DeleteBook(int id)
+        {
+            var existingBook = _books.Find(b => b.Id == id);
+
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            _books.Remove(existingBook);
+
+            return Ok();
+        }
+
+        private int GenerateNewId()
+        {
+            return _books.Count + 1;
+        }
     }
-
-    private int GenerateNewId()
-    {
-        return _books.Count + 1;
-    }
-
 }
